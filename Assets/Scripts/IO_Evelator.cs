3c60ac9d9bef1ef5454aad22d9a6018aa18b506c
Gonzalo Pérez Romero <Glacegon1876@gmail.com>
2025-01-26 14:19:17 +0100

    Build 1

--- Assets/Scripts/IO_Evelator.cs
+++ Assets/Scripts/IO_Evelator.cs
@@ -6,13 +6,15 @@ public class IO_Evelator : InteractableObject
     public float heightMax = 2.0f;
     public GameObject platform;
     public AudioSource sound;
+    //public Animator anim; //Arrastrar plataforma
     public override void Action(){
         sound.Play();
-        Collider[] colliders = Physics.OverlapSphere(platform.transform.position, platform.transform.GetComponent<BoxCollider>().size.y*2, LayerMask.GetMask("Player"));
+        /*Collider[] colliders = Physics.OverlapSphere(platform.transform.position, platform.transform.GetComponent<BoxCollider>().size.y*2, LayerMask.GetMask("Player"));
         foreach(Collider hitCollider in colliders){
             if(hitCollider.gameObject.name == "Player") StartCoroutine(Elevator(2.5f, hitCollider.gameObject));
         }
-        StartCoroutine(ElevatorSolo(2.5f));
+        //StartCoroutine(ElevatorSolo(2.5f));*/
+        //anim.SetBool("name", true);
     }
 
     IEnumerator ElevatorSolo(float time){
